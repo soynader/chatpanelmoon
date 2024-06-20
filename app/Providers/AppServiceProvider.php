6926bf5533f3e9d7725151dbdc80d677ac9061f1
TecnoChat <76270452+soynader@users.noreply.github.com>
2024-06-20 09:30:08 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -16,9 +16,12 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     * 
+     * @return void
      */
- public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-        //
+        $url->forceScheme('https');
     }
 }
+
