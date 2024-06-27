465c511459f3ee1cc5becd253294d1a718933e15
sofucox <sofyreinel@gmail.com>
2024-06-27 08:23:45 -0500

    nombre de app

--- vendor/moonshine/moonshine/config/moonshine.php
+++ vendor/moonshine/moonshine/config/moonshine.php
@@ -12,7 +12,7 @@ return [
     'dir' => 'app/MoonShine',
     'namespace' => 'App\MoonShine',
 
-    'title' => env('MOONSHINE_TITLE', 'MoonShine'),
+    'title' => env('MOONSHINE_TITLE', 'ChatMax'),
     'logo' => env('MOONSHINE_LOGO'),
     'logo_small' => env('MOONSHINE_LOGO_SMALL'),
 
